da976022627719a4461a0c1804ae6198ee8a25cc
Flacko <f@ak68a.co>
2023-06-15 21:54:20 -0400

    Initial Commit

---
+++ PlateCalc/PlateCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlateCalcApp.swift
+//  PlateCalc
+//
+//  Created by Ayo Moreira on 6/15/23.
+//
+
+import SwiftUI
+
+@main
+struct PlateCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
